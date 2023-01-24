01923f2fbd19278d5a2f88c3943de6d27c6ddc37
cindy dutrait <cindy.dutrait@laposte.net>
2023-01-24 11:07:46 +0100

    modif user / admin

--- src/pages/EspaceUser.jsx
+++ src/pages/EspaceUser.jsx
@@ -9,13 +9,20 @@ import { useNavigate } from "react-router";
 const EspaceUser = () => {
 
     const navigate = useNavigate();
+    //use navigate est importé de la librairie react router et permet
+    //de naviguer entre les différentes routes de l'application
 
     const [role, setRole] = useState(null);
 
     useEffect(() => {
 
         const jwtLocalStorage = localStorage.getItem('jwt');
+        //localStorage récupère la valeure stockée dans le navigateur
+        //getItem est la méthode ets utilisée pour récupérer la valeur du jwt 
+        //Tout ceci est stockée dans la const jwtLocalStorage  
         const jwtRole = JSON.parse(jwtLocalStorage).role;
+        //Méthode qui converti la chaine de caractère stockée dans jwtLocalStorage
+        //un objet JS puis récupère role pour la stocké dans jwtRole
   
             setRole(jwtRole);
 
